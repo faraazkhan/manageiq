8642b21ff5e425eed2cbffbaedf3c21442e5a23a
Joe Rafaniello <jrafanie@redhat.com>
2018-10-05 17:07:56 -0400

    Rails 5.1 changed arity from 1 to 0.
    It was changed in rails/rails@a553302

--- lib/extensions/ar_virtual.rb
+++ lib/extensions/ar_virtual.rb
@@ -198,7 +198,7 @@ module VirtualDelegates
         if to_ref.macro == :has_one || to_ref.macro == :belongs_to
           blk = ->(arel) { arel.limit = 1 } if to_ref.macro == :has_one
           lambda do |t|
-            join_keys = to_ref.join_keys(to_ref.klass)
+            join_keys = to_ref.join_keys
             src_model_id = arel_attribute(join_keys.foreign_key, t)
             VirtualDelegates.select_from_alias(to_ref, col, join_keys.key, src_model_id, &blk)
           end
